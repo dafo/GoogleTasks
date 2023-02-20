afeb408874ea9ab36bc1c077fc0e9d6c00a5a3eb
Alexander M <niron_hsc@yahoo.com>
2023-02-20 11:12:38 +0200

    Initial commit

---
+++ GoogleTasks/Controllers/TasksController.cs
@@ -0,0 +1,120 @@
+﻿using GoogleTasks.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using Task = GoogleTasks.Models.Task;
+
+namespace GoogleTasks.Controllers
+{
+    public class TasksController : Controller
+    {
+        private readonly TaskContext _dbContext;
+        public TasksController(TaskContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        //GET: /{tasklist}/{task}
+        [HttpGet("{tasklist}/{task}")]
+        public async Task<GoogleTasks.Models.Task?> GetTask(string tasklist, int task)
+        {
+            if (_dbContext.Tasks == null)
+            {
+                return null;
+            }
+
+            var t = await _dbContext.Tasks.FindAsync(task);
+
+            if (t == null || t.Parent != tasklist)
+            {
+                return null;
+            }
+
+            return t;
+        }
+
+        //POST: /{tasklist}
+        [HttpPost("task")]
+        public async Task<GoogleTasks.Models.Task?> InsertTask(Task task)
+        {
+            _dbContext.Tasks.Add(task);
+            await _dbContext.SaveChangesAsync();
+
+            return task;
+        }
+
+        //PUT: /{tasklist}/{task}
+        [HttpPut("id")]
+        public async Task<GoogleTasks.Models.Task?> UpdateTask(int id, Task task)
+        {
+            if (id != task.Id)
+            {
+                return null;
+            }
+            _dbContext.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return null;
+        }
+
+        //DELETE: /{tasklist}/{task}
+        [HttpDelete("{tasklist}/{task}")]
+        public async Task<GoogleTasks.Models.Task?> DeleteTask(string tasklist, int task)
+        {
+            if (_dbContext.Tasks == null)
+            {
+                return null;
+            }
+
+            var t = await _dbContext.Tasks.FindAsync(task);
+
+            if (t == null || t.Parent != tasklist)
+            {
+                return null;
+            }
+
+            _dbContext.Tasks.Remove(t);
+            await _dbContext.SaveChangesAsync();
+
+            return null;
+        }
+
+        //LIST: /{tasklist}/
+        [HttpGet("{tasklist}")]
+        public async Task<ICollection<GoogleTasks.Models.Task>> ListTasks(string tasklist, string status, bool deleted)
+        {
+            if (_dbContext.Tasks == null)
+            {
+                return new List<Task>();
+            }
+
+            if (!_dbContext.Tasks.AsQueryable().Where(t => t.Parent == tasklist).Any())
+            {
+                return new List<Task>();
+            }
+
+            var t = _dbContext.Tasks.AsQueryable().Where(t => t.Parent == tasklist).First();
+
+            return new List<Task> { t };
+        }
+
+        private bool TaskExists(int id)
+        {
+            return (_dbContext.Tasks?.Any(t => t.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ GoogleTasks/Migrations/20230208135351_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoogleTasks.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Parent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SelfLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Due = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Deleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
